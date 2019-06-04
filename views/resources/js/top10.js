e5d9c50c531fa0cd8f7e7f3965a4d26bd0899053
Szwajowy <krzycza96@gmail.com>
2019-06-04 21:54:47 +0200

    Minor fixes

--- views/resources/js/top10.js
+++ views/resources/js/top10.js
@@ -3,7 +3,7 @@ var duoTable = document.getElementById('top10-duo');
 var squadTable = document.getElementById('top10-squad');
 
 async function getUserInfo(userNickname) {
-    return await fetch('http://localhost:3000/statistics_by_nickname/'+JSON.stringify(userNickname), {
+    return await fetch('http://localhost:3000/statistics_by_nickname/'+escape(userNickname), {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
@@ -12,19 +12,44 @@ async function getUserInfo(userNickname) {
     .then((res) => res.json())
     .then((data) => {
         var playerData = data.stats;
-        if(!playerData)
-            return null;
+        if(!playerData) {
+            playerData = {
+                nickname: 'Nickname',
+                solo: {
+                    kills: 0,
+                    matchesplayed: 0,
+                    placetop1: 0,
+                    placetop10: 0,
+                    placetop25: 0
+                },
+                duo: {
+                    kills: 0,
+                    matchesplayed: 0,
+                    placetop1: 0,
+                    placetop6: 0,
+                    placetop12: 0
+                },
+                squad: {
+                    kills: 0,
+                    matchesplayed: 0,
+                    placetop1: 0,
+                    placetop3: 0,
+                    placetop6: 0
+                },
+            }
+        }
 
         console.log(playerData);
         return playerData;
     });
 }
 
-async function displayUserInfo(type, playerData) {
+async function displayUserInfo(type, playerData, index) {
     switch(type) {
         case 'solo':
             row = '<tr>'+
-                '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
+                    '<td>'+index+'</td>'+
+                    '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
                     '<td class="kills">'+playerData.solo.kills+'</td>'+
                     '<td class="deaths">'+(playerData.solo.matchesplayed - playerData.solo.placetop1)+'</td>'+
                     '<td class="kd">'+(playerData.solo.kills / (playerData.solo.matchesplayed - playerData.solo.placetop1)).toFixed(2)+'</td>'+
@@ -38,24 +63,31 @@ async function displayUserInfo(type, playerData) {
             break;
         case 'duo':
             row = '<tr>'+
-                '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
+                    '<td>'+index+'</td>'+
+                    '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
                     '<td class="kills">'+playerData.duo.kills+'</td>'+
                     '<td class="deaths">'+(playerData.duo.matchesplayed - playerData.duo.placetop1)+'</td>'+
                     '<td class="kd">'+(playerData.duo.kills / (playerData.duo.matchesplayed - playerData.duo.placetop1)).toFixed(2)+'</td>'+
                     '<td class="games">'+playerData.duo.matchesplayed+'</td>'+
                     '<td class="wins">'+playerData.duo.placetop1+'</td>'+
-                    '<td class="top6">'+playerData.duo.placetop6+'</td>'+
+                    '<td class="top5">'+playerData.duo.placetop5+'</td>'+
                     '<td class="top12">'+playerData.duo.placetop12+'</td>'+
                 '</tr>';
 
             duoTable.innerHTML += row;
             break;
         case 'squad':
+            deaths = (playerData.squad.matchesplayed - playerData.squad.placetop1);
+            kd = 0;
+            if(deaths != 0)
+                kd = (playerData.squad.kills / deaths).toFixed(2);
+
             row = '<tr>'+
-                '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
+                    '<td>'+index+'</td>'+
+                    '<th scope="row" class="nick">'+playerData.nickname+'</th>'+
                     '<td class="kills">'+playerData.squad.kills+'</td>'+
-                    '<td class="deaths">'+(playerData.squad.matchesplayed - playerData.squad.placetop1)+'</td>'+
-                    '<td class="kd">'+(playerData.squad.kills / (playerData.squad.matchesplayed - playerData.squad.placetop1)).toFixed(2)+'</td>'+
+                    '<td class="deaths">'+deaths+'</td>'+
+                    '<td class="kd">'+kd+'</td>'+
                     '<td class="games">'+playerData.squad.matchesplayed+'</td>'+
                     '<td class="wins">'+playerData.squad.placetop1+'</td>'+
                     '<td class="top3">'+playerData.squad.placetop3+'</td>'+
@@ -68,7 +100,7 @@ async function displayUserInfo(type, playerData) {
 }
 
 async function getTop10(type) {
-    fetch('http://localhost:3000/top_10_players/'+JSON.stringify(type), {
+    return await fetch('http://localhost:3000/top_10_players/'+escape(type), {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
@@ -78,9 +110,11 @@ async function getTop10(type) {
     .then(async (data) => {
         console.log(data);
         
-        for(let player of data.rank) {
-            var playerData = await getUserInfo(player);
-            await displayUserInfo(data.type, playerData);
+        for(let [index, player] of data.rank.entries()) {
+            var playerData;
+            playerData = await getUserInfo(player);
+
+            await displayUserInfo(data.type, playerData, index+1);
         };
     });
 }
@@ -89,7 +123,7 @@ function getTop10from(country) {
     if(!country)
         return new Error('Country is required parameter!');
 
-    fetch('http://localhost:3000/top_10_players_by_country/'+country, {
+    fetch('http://localhost:3000/top_10_players_by_country/'+escape(country), {
         method: 'POST',
         headers: {
             'Content-type': 'application/json'
