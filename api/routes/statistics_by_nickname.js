6654a4a6453fe385d92307f43a595238d80420af
DawidMilka <dawid.milka@interia.com>
2019-06-04 18:01:28 +0200

    Bug fixes 2.2

--- api/routes/statistics_by_nickname.js
+++ api/routes/statistics_by_nickname.js
@@ -49,65 +49,76 @@ router.post('/:username', (req, res, next) => {
     headers: {'Authorization': "84bf0cc2b4b6be0918da79eff3a8e93b"}
   })
   .then(docss => {
-    // res.status(200).json(docss);
-    axios({
-      method: 'get',
-      url: `https://fortnite-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=${docss.data.data.uid}`,
-      headers: {'Authorization': "84bf0cc2b4b6be0918da79eff3a8e93b"}
-    })
-    .then(docs => {
 
-      let solo, duo, squad;
-      for(let i = 0; i < docs.data.data.stats.keyboardmouse.length; i++ ) {
-        if(docs.data.data.stats.keyboardmouse[i].id === 'defaultsolo') {
-          solo = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
-        }
-        if(docs.data.data.stats.keyboardmouse[i].id === 'defaultduo') {
-          duo = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
-        }
-        if(docs.data.data.stats.keyboardmouse[i].id === 'defaultsquad') {
-          squad = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
-        }
-      }
-
-      const today = new Date().toLocaleString();
-      // const data = res.status(200).json(docs.data);
-      const player =  new Player({
-              _id: new mongoose.Types.ObjectId(),
-              nickname: docs.data.data.username,
-              solo: solo,
-              duo: duo,
-              squad: squad,
-              query_date: today
-
-      });
-      // res.status(200).json(docs.data);
-      // res.status(200).json(player);
-
-      player
-      .save()
-      .then(result => {
-        console.log(result);
-        res.status(201).json({
-          message: `Selected stats for player ${docs.data.data.username}`,
-          stats: player
-        });
+      axios({
+        method: 'get',
+        url: `https://fortnite-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id=${docss.data.data.uid}`,
+        headers: {'Authorization': "84bf0cc2b4b6be0918da79eff3a8e93b"}
       })
-      .catch( err => {
-        console.log(err);
+      .then(docs => {
+
+        let solo, duo, squad;
+        for(let i = 0; i < docs.data.data.stats.keyboardmouse.length; i++ ) {
+          if(docs.data.data.stats.keyboardmouse[i].id === 'defaultsolo') {
+            solo = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
+          }
+          if(docs.data.data.stats.keyboardmouse[i].id === 'defaultduo') {
+            duo = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
+          }
+          if(docs.data.data.stats.keyboardmouse[i].id === 'defaultsquad') {
+            squad = docs.data.data.stats.keyboardmouse[i].entries[0].stats;
+          }
+        }
+
+        const today = new Date().toLocaleString();
+        // const data = res.status(200).json(docs.data);
+        const player =  new Player({
+                _id: new mongoose.Types.ObjectId(),
+                nickname: docs.data.data.username,
+                solo: solo,
+                duo: duo,
+                squad: squad,
+                query_date: today
+
+        });
+        // res.status(200).json(docs.data);
+        // res.status(200).json(player);
+
+        player
+        .save()
+        .then(result => {
+          console.log(result);
+          res.status(201).json({
+            message: `Selected stats for player ${docs.data.data.username}`,
+            stats: player
+          });
+        })
+        .catch( err => {
+          console.log(err);
+          res.status(500).json({
+            message: `Stats for player ${docs.data.data.username} failed to search`,
+            error: err
+          });
+        });
+        //
+
+      })
+      .catch(e => {
         res.status(500).json({
-          message: `Stats for player ${docs.data.data.username} failed to search`,
+          message: `Player doesnt exist`,
           error: err
         });
+        console.log(e);
       });
-      //
 
-    })
-    .catch(e => {
-      console.log(e);
-    });
+
   })
   .catch(ee => {
+    res.status(500).json({
+      message: `Player doesnt exist`,
+
+
+    });
     console.log(ee);
   }) ;
 
